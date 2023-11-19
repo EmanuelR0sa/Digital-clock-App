900fea3f9dfef188748125ebf7c88bc6d3ad651d
Emanuel Rosa <emanuelnzinga@gmail.com>
2023-11-19 22:54:32 +0000

    add new elements

--- clockScript.js
+++ clockScript.js
@@ -1,15 +1,17 @@
 function digitalClock() {
   const myClock = document.getElementById("clockElement");
-
+  const weekDays = document.getElementById("weekElement");
   const clock = new Date();
 
   const hour = clock.getHours();
   const minutes = clock.getMinutes();
   const seconds = clock.getSeconds();
+  const week = clock.getDay().toLocaleString("default");
 
-  console.log(`${hour}:${minutes}.${seconds}`);
+  //console.log(`${hour}:${minutes}.${seconds}`);
 
   myClock.innerHTML = `${hour}:${minutes}:${seconds}`;
+  weekDays.innerHTML = `${week}`;
 }
 digitalClock();
 setInterval(digitalClock, 1000);
