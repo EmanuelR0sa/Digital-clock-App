f1998cd48382212e85f8d894d5dfecb3980db477
Emanuel Rosa <emanuelnzinga@gmail.com>
2023-11-23 18:24:35 +0000

    new update

--- clockScript.js
+++ clockScript.js
@@ -1,17 +1,33 @@
 function digitalClock() {
   const myClock = document.getElementById("clockElement");
-  const weekDays = document.getElementById("weekElement");
+  const weekDayEl = document.getElementById("weekElement");
+  const dayEl = document.getElementById("dayElement");
+  const monthEl = document.getElementById("monthElement");
+
   const clock = new Date();
 
   const hour = clock.getHours();
   const minutes = clock.getMinutes();
   const seconds = clock.getSeconds();
-  const week = clock.getDay().toLocaleString("default");
+  const dayOfWeek = clock.getDay();
+  const dayOfMonth = clock.getDate();
+  const month = clock.getMonth();
+  const weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
+  const currentDayOfWeek = weekdays[dayOfWeek];
   //console.log(`${hour}:${minutes}.${seconds}`);
 
   myClock.innerHTML = `${hour}:${minutes}:${seconds}`;
-  weekDays.innerHTML = `${week}`;
+  weekDayEl.innerHTML = `${currentDayOfWeek}`;
+  dayEl.innerHTML = `${dayOfMonth}`;
 }
 digitalClock();
 setInterval(digitalClock, 1000);
