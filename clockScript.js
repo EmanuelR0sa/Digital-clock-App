57fcba1b00e719d0983f759edddd749e927d2b49
Emanuel Rosa <emanuelnzinga@gmail.com>
2023-11-24 11:15:58 +0000

    ui updates

--- clockScript.js
+++ clockScript.js
@@ -50,3 +50,30 @@ function digitalClock() {
 }
 digitalClock();
 setInterval(digitalClock, 1000);
+
+const icon = document.getElementById("icon");
+const hideClock = document.getElementById("Container");
+const hidedayWM = document.getElementById("dayWMContainer");
+
+icon.addEventListener("click", () => {
+  document.body.classList.toggle("pomodoro");
+
+  if (document.body.classList.contains("pomodoro")) {
+    icon.className = "fa-solid fa-clock";
+    hideClock.className = "hidden";
+  } else {
+    icon.className = "fa-solid fa-stopwatch";
+    hideClock.className = "view";
+  }
+});
+
+/*
+hideClock.addEventListener("click", () => {
+  document.body.classList.toggle('hidden');
+  if (document.body.classList.contains("pomodoro")) {
+    icon.className = "fa-solid fa-clock";
+  } else {
+    icon.className = "fa-solid fa-stopwatch";
+  }
+});
+*/
